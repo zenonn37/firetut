5a33a8449993a2953de85cc73a1c2a9df595f027
chris crowd <zent29@gmail.com>
2014-04-30 17:00:37 -0400

    help app

---
+++ scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+/* global app:true */
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/posts.html',
+        controller: 'PostsCtrl'
+      })
+      .when('/posts/:postId', {
+        templateUrl: 'views/showpost.html',
+        controller: 'PostViewCtrl'
+      })
+      .when('/register', {
+        templateUrl: 'views/register.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'AuthCtrl'
+      })
+      .when('/users/:username', {
+        templateUrl: 'views/profile.html',
+        controller: 'ProfileCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+.constant('FIREBASE_URL', 'https://angtuts.firebaseio.com/');
